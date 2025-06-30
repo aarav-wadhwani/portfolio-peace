55c370e6931f2b4bc7612d8105c00c7d28f9e554
Aarav Wadhwani <66585793+aarav-wadhwani@users.noreply.github.com>
2025-06-30 17:56:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Simple Stock Tracker! 🚀</h1>
+        <p>React app deployed to the cloud.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
